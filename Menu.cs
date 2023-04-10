b9b01bae6351bf691f6f9f0f20c0435afd667909
robolev <92596597+robolev@users.noreply.github.com>
2023-04-10 21:16:10 +0200

    InitialCommit

---
+++ Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Figgle;
+
+namespace SeaBattle
+{
+    public class Menu
+    {
+        public Menu() 
+        {
+            DrawMenu();
+        }
+        public void DrawMenu()
+        {
+            Console.WriteLine(FiggleFonts.Standard.Render("SeaBattle  \\__/ "));
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+           Menu menu = new Menu();
            
         }
     }
